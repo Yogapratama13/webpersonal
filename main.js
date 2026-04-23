b1dbe4852a1e7b7ab49ca0d34068575e4d951b67
yoga pratama <yogapratama@MacBook-Air-yoga.local>
2026-04-23 10:11:06 +0700

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+import Typed from "typed.js";
+
+const typed = new Typed("#textRotator", {
+  strings: [
+    "Build functional apps",
+    "Automate your workflow",
+    "Lead seamless projects",
+    "Drive digital growth",
+  ],
+  typeSpeed: 100,
+  loop: true,
+});
